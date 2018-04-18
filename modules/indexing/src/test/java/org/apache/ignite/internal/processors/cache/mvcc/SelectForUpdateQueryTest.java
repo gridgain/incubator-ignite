0d0878972aca84b0657dded8337cc5e114ed5127
Alexander Paschenko <alexander.a.paschenko@gmail.com>
2018-04-18 18:37:42 +0300

    IGNITE-6937 Contd

--- modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/SelectForUpdateQueryTest.java
+++ modules/indexing/src/test/java/org/apache/ignite/internal/processors/cache/mvcc/SelectForUpdateQueryTest.java
@@ -18,18 +18,19 @@
 package org.apache.ignite.internal.processors.cache.mvcc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
+import javax.cache.Cache;
 import org.apache.ignite.Ignite;
-import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.cache.CachePeekMode;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
-import org.apache.ignite.configuration.CacheConfiguration;
-import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.internal.IgniteEx;
-import org.apache.ignite.internal.processors.cache.query.SqlFieldsQueryEx;
+import org.apache.ignite.internal.processors.cache.IgniteInternalCache;
+import org.apache.ignite.internal.processors.cache.distributed.near.GridNearTxLocal;
+import org.apache.ignite.internal.processors.cache.transactions.IgniteTxKey;
 import org.apache.ignite.internal.util.typedef.X;
 
 import static org.apache.ignite.internal.processors.cache.index.AbstractSchemaSelfTest.connect;
@@ -40,22 +41,10 @@ import static org.apache.ignite.internal.processors.cache.index.AbstractSchemaSe
  */
 public class SelectForUpdateQueryTest extends CacheMvccAbstractTest {
     /** {@inheritDoc} */
-    @Override protected void beforeTestsStarted() throws Exception {
-        super.beforeTestsStarted();
-
-        startGrids(3);
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void afterTestsStopped() throws Exception {
-        stopAllGrids();
-
-        super.afterTestsStopped();
-    }
-
-    /** {@inheritDoc} */
     @Override protected void beforeTest() throws Exception {
         super.beforeTest();
+
+        startGrids(3);
 
         try (Connection c = connect(grid(0))) {
             execute(c, "create table person (id int primary key, firstName varchar, lastName varchar) " +
@@ -75,50 +64,56 @@ public class SelectForUpdateQueryTest extends CacheMvccAbstractTest {
         super.afterTest();
     }
 
-    /** {@inheritDoc} */
-    @Override protected IgniteConfiguration getConfiguration(String gridName) throws Exception {
-        IgniteConfiguration cfg = super.getConfiguration(gridName);
-
-        cfg.setMvccEnabled(true);
-
-        CacheConfiguration ccfg = new CacheConfiguration("mvcc*");
-
-        ccfg.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
-
-        return cfg;
-    }
-
     /**
      *
      */
-    public void testSelectForUpdate() throws SQLException {
-        try (Connection c = connect(grid(0))) {
-            c.setAutoCommit(false);
+    public void testSelectForUpdate() throws IgniteCheckedException {
+        IgniteEx node = grid(0);
 
-            try (PreparedStatement ps = c.prepareStatement("select * from person  order by id for update")) {
-                ps.setFetchSize(10);
+        IgniteCache<Integer, ?> cache = node.cache("Person");
 
-                try (ResultSet rs = ps.executeQuery()) {
-                    while (rs.next())
-                        X.println(rs.getObject(1).toString());
-                }
+        cache.query(new SqlFieldsQuery("begin"));
+
+        try {
+            SqlFieldsQuery qry = new SqlFieldsQuery("select id from person order by id for update").setPageSize(10);
+
+            List<List<?>> res = cache.query(qry).getAll();
+
+            assertEquals(300, res.size());
+
+            GridNearTxLocal tx = MvccUtils.activeTx(node.context());
+
+            assertNotNull(tx);
+
+            IgniteInternalCache<Integer, ?> cachex = node.cachex("Person").keepBinary();
+
+            for (Cache.Entry e : cachex.localEntries(new CachePeekMode[]{ CachePeekMode.PRIMARY })) {
+                assertNotNull(tx.txState().writeMap()
+                    .get(new IgniteTxKey(cachex.context().toCacheKeyObject(e.getKey()), cachex.context().cacheId())));
             }
+        }
+        finally {
+            node.cache("Person").query(new SqlFieldsQuery("rollback"));
         }
     }
 
     /**
      *
      */
-    public void testSelectForUpdateLocal() throws SQLException {
-        Ignite node = ignite(0);
+    public void testSelectForUpdateLocal() {
+        Ignite node = grid(0);
 
-        SqlFieldsQueryEx qry = new SqlFieldsQueryEx("select * from person order by id for update", true).setLocal(true);
+        node.cache("Person").query(new SqlFieldsQuery("begin"));
 
-        qry.setAutoCommit(false);
+        SqlFieldsQuery qry = new SqlFieldsQuery("select id from person order by id for update").setLocal(true);
 
         List<List<?>> res = node.cache("Person").query(qry).getAll();
 
+        Set<Integer> keys = new HashSet<>();
+
         for (List<?> r : res)
             X.println(r.get(0).toString());
+
+        node.cache("Person").query(new SqlFieldsQuery("rollback"));
     }
 }
