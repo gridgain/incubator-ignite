7279f0d302c08e3ee885a8e9263a498269148d32
dspavlov <dpavlov.spb@gmail.com>
2018-06-07 18:07:44 +0300

    IGNITE-8742 Failing test with reference to issue

--- modules/direct-io/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/wal/IgniteNativeIoWalFlushFsyncSelfTest.java
+++ modules/direct-io/src/test/java/org/apache/ignite/internal/processors/cache/persistence/db/wal/IgniteNativeIoWalFlushFsyncSelfTest.java
@@ -14,13 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.ignite.internal.processors.cache.persistence;
+package org.apache.ignite.internal.processors.cache.persistence.db.wal;
 
-import org.apache.ignite.internal.processors.cache.persistence.db.wal.IgniteWalFlushFsyncSelfTest;
-
+/**
+ * Native IO version of test
+ */
 public class IgniteNativeIoWalFlushFsyncSelfTest extends IgniteWalFlushFsyncSelfTest {
     /** {@inheritDoc} */
     @Override public void testFailAfterStart() throws Exception {
+        fail("https://issues.apache.org/jira/browse/IGNITE-8742");
+
         super.testFailAfterStart();
     }
 }
