e43b933bb909ceb43fa2d63b4ebd99d785d5c40c
Alexey Kukushkin <AlexeyKukushkin@yahoo.com>
2018-08-07 00:34:38 +0300

    initial revision

---
+++ examples/src/main/java/com/anuta/ncxignite/extension/affinity/NcxAffinityFunction.java
@@ -0,0 +1,6 @@
+package com.anuta.ncxignite.extension.affinity;
+
+import org.apache.ignite.cache.affinity.rendezvous.RendezvousAffinityFunction;
+
+public class NcxAffinityFunction extends RendezvousAffinityFunction {
+}
