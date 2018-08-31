8004b6cb75d81b08c382a652b637b743eda4d569
Ignite Teamcity <ignite@apache.org>
2018-08-31 15:58:33 +0000

    version changed to: 2.5.1-p13

--- modules/platforms/dotnet/Apache.Ignite.EntityFramework/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.EntityFramework/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("c558518a-c1a0-4224-aaa9-a8688474b4dc")]
 
-[assembly: AssemblyVersion("2.5.0.26265")]
-[assembly: AssemblyFileVersion("2.5.0.26265")]
-[assembly: AssemblyInformationalVersion("2.5.0")]
+[assembly: AssemblyVersion("2.5.1.31384")]
+[assembly: AssemblyFileVersion("2.5.1.31384")]
+[assembly: AssemblyInformationalVersion("2.5.1-p13")]
 
 [assembly: CLSCompliant(true)]
 
