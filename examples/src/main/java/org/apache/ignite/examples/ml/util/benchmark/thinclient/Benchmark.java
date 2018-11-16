65ca902887b0d48eb5738e94e997e8ea4a5ec363
Alexey Platonov <aplatonovv@gmail.com>
2018-11-16 13:14:39 +0300

    add pretty table

--- examples/src/main/java/org/apache/ignite/examples/ml/util/benchmark/thinclient/Benchmark.java
+++ examples/src/main/java/org/apache/ignite/examples/ml/util/benchmark/thinclient/Benchmark.java
@@ -18,6 +18,7 @@
 package org.apache.ignite.examples.ml.util.benchmark.thinclient;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -31,52 +32,98 @@ import org.apache.ignite.examples.ml.util.benchmark.thinclient.utils.Measure;
  */
 public class Benchmark {
     private static final int SAMPLES = 20;
-    private static final int[] PAGE_SIZES = new int[] {5/*, 10, 20, 50, 100, 150, 200, 300, 400, 500, 600*/};
+    private static final int[] PAGE_SIZES = new int[] {5, 10, 20, 50, 100, 150, 200, 300, 400, 500, 600};
 
-    private static final int THREAD_COUNT = 10;
+    private static final int MAX_THREAD_COUNT = 8;
     public static final boolean DISTINGUISH_PARTITIONS = true;
     public static final boolean USE_FILTER = false;
 
     static {
-        assert THREAD_COUNT <= ServerMock.COUNT_OF_PARTITIONS : "THREAD_COUNT <= ServerMock.COUNT_OF_PARTITIONS";
+        assert MAX_THREAD_COUNT <= ServerMock.COUNT_OF_PARTITIONS : "THREAD_COUNT <= ServerMock.COUNT_OF_PARTITIONS";
         assert (!DISTINGUISH_PARTITIONS && !USE_FILTER) || DISTINGUISH_PARTITIONS ^ USE_FILTER : "(!DISTINGUISH_PARTITIONS && !USE_FILTER) || DISTINGUISH_PARTITIONS ^ USE_FILTER";
     }
 
-    private static ExecutorService POOL = Executors.newFixedThreadPool(THREAD_COUNT);
+    private static ExecutorService POOL;
 
     public static AtomicLong downloadedBytes = new AtomicLong(0L);
 
     public static void main(String... args) throws Exception {
         Long start = System.currentTimeMillis();
-        Long end = 0L;
-        try {
-            for (int pageSize : PAGE_SIZES) {
-                ArrayList<Measure> times = new ArrayList<>(SAMPLES);
-                for (int i = 0; i < SAMPLES; i++)
-                    times.add(oneMeasure(pageSize, USE_FILTER, DISTINGUISH_PARTITIONS));
 
-                Measure.computeStatsAndPrint(pageSize, times);
+        List<MeasureWithMeta> benchMeta = new ArrayList<>();
+
+        for (int threadCount = 1; threadCount <= MAX_THREAD_COUNT; ) {
+            try {
+                POOL = Executors.newFixedThreadPool(MAX_THREAD_COUNT);
+
+                for (int pageSize : PAGE_SIZES) {
+                    long alreadyDownloadedKBytes = downloadedBytes.get() / 1024;
+                    ArrayList<Measure> times = new ArrayList<>(SAMPLES);
+                    for (int i = 0; i < SAMPLES; i++)
+                        times.add(oneMeasure(pageSize, USE_FILTER, DISTINGUISH_PARTITIONS));
+
+                    System.out.println(String.format("measure [page size = %d, thread count = %d, downloaded kbytes = %d]",
+                        pageSize, threadCount, (downloadedBytes.get() / 1024) - alreadyDownloadedKBytes
+                    ));
+                    Measure.computeStatsAndPrint(pageSize, times);
+
+                    benchMeta.add(new MeasureWithMeta(threadCount, pageSize, times));
+                }
             }
-            end = System.currentTimeMillis();
-        } finally {
-            POOL.shutdown();
-            POOL.awaitTermination(1, TimeUnit.DAYS);
-            System.out.println(String.format("Done [downloaded kbytes = %d by %d s]", downloadedBytes.get() / 1024, (end - start) / 1000));
+            finally {
+                POOL.shutdown();
+                POOL.awaitTermination(1, TimeUnit.DAYS);
+            }
+
+            threadCount = Math.min(MAX_THREAD_COUNT + 1, threadCount * 2);
         }
+
+        Long end = System.currentTimeMillis();
+        System.out.println(String.format("Done [downloaded kbytes = %d in %d sec.]", downloadedBytes.get() / 1024, (end - start) / 1000));
+        System.out.println();
+        printTable(benchMeta);
     }
 
     private static Measure oneMeasure(int pageSize, boolean useFilter, boolean distinguishPartitions) throws Exception {
-        ArrayList<Future<Optional<Measure>>> futures = new ArrayList<>(THREAD_COUNT);
-        for(int i = 0; i < THREAD_COUNT; i++) {
+        ArrayList<Future<Optional<Measure>>> futures = new ArrayList<>(MAX_THREAD_COUNT);
+        for (int i = 0; i < MAX_THREAD_COUNT; i++) {
             final int clientID = i;
             futures.add(POOL.submit(() -> {
-                return new ThinClientMock(pageSize, clientID, THREAD_COUNT, useFilter, distinguishPartitions).measure();
+                return new ThinClientMock(pageSize, clientID, MAX_THREAD_COUNT, useFilter, distinguishPartitions).measure();
             }));
         }
 
         ArrayList<Measure> bucket = new ArrayList<>(futures.size());
-        for(Future<Optional<Measure>> f : futures)
+        for (Future<Optional<Measure>> f : futures)
             f.get().ifPresent(bucket::add);
         return Measure.sumOf(bucket);
+    }
+
+    private static class MeasureWithMeta {
+
+        private final List<Measure> measures;
+        private final int threadCount;
+        private final int pageSize;
+
+        public MeasureWithMeta(int threadCount, int pageSize, List<Measure> measures) {
+            this.measures = measures;
+            this.threadCount = threadCount;
+            this.pageSize = pageSize;
+        }
+    }
+
+    private static void printTable(List<MeasureWithMeta> benchMeta) {
+        StringBuilder header = new StringBuilder("thread count\tpage size");
+        Measure.tableHeader().forEach(name -> header.append("\t" + name));
+        System.out.println(header);
+        benchMeta.forEach(meta -> {
+            StringBuilder row = new StringBuilder();
+            row.append(meta.threadCount).append("\t")
+                .append(meta.pageSize).append("\t");
+            Measure.computeAllMetrics(meta.measures).forEach(r -> {
+               row.append(r.mean).append("\t");
+            });
+            System.out.println(row);
+        });
     }
 }
--- examples/src/main/java/org/apache/ignite/examples/ml/util/benchmark/thinclient/utils/Measure.java
+++ examples/src/main/java/org/apache/ignite/examples/ml/util/benchmark/thinclient/utils/Measure.java
@@ -20,9 +20,14 @@ package org.apache.ignite.examples.ml.util.benchmark.thinclient.utils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Measure {
+    private static List<Meta> measuresMetaToPrint = Arrays.asList(
+        new Meta("latency", Measure::getLatency, "ms"),
+        new Meta("throughput", Measure::getThroughput, "kbytes/s")
+    );
+
     private long latency;
     private long throughput;
 
@@ -51,28 +56,25 @@ public class Measure {
         private final String name;
         private final Function<Measure, Long> getter;
         private final String dim;
-
         public Meta(String name,
             Function<Measure, Long> getter, String dim) {
             this.name = name;
             this.getter = getter;
             this.dim = dim;
         }
+
+    }
+    public static void computeStatsAndPrint(long pageSize, List<Measure> measures) {
+        measuresMetaToPrint.forEach(t -> {
+            MeasuringResult res = computeMetric(measures, t.getter);
+            System.out.printf("Page size %d Mb, %s = %.2f ± %.2f %s\n", pageSize, t.name, res.mean, res.std, t.dim);
+        });
     }
 
-    public static void computeStatsAndPrint(long pageSize, List<Measure> measures) {
-        List<Meta> measuresMetaToPrint = Arrays.asList(
-            new Meta("latency", Measure::getLatency, "ms"),
-            new Meta("throughput", Measure::getThroughput, "kbytes/s")
-        );
-
-        measuresMetaToPrint.forEach(t -> {
-            double mean = mean(measures, t.getter);
-            double stddev = std(measures, t.getter);
-            double interval95 = 1.96 * stddev / Math.sqrt(measures.size());
-
-            System.out.printf("Page size %d Mb, %s = %.2f ± %.2f %s\n", pageSize, t.name, mean, interval95, t.dim);
-        });
+    public static List<MeasuringResult> computeAllMetrics(List<Measure> measures) {
+        return measuresMetaToPrint.stream()
+            .map(t -> computeMetric(measures, t.getter))
+            .collect(Collectors.toList());
     }
 
     private static double mean(Iterable<Measure> measures, Function<Measure, Long> getter) {
@@ -99,5 +101,27 @@ public class Measure {
         }
 
         return Math.sqrt(accumulatedValue / counter);
+    }
+
+    public static List<String> tableHeader() {
+        return measuresMetaToPrint.stream().map(x -> x.name + "(" + x.dim + ")").collect(Collectors.toList());
+    }
+
+    private static MeasuringResult computeMetric(List<Measure> measures, Function<Measure, Long> getter) {
+        double mean = mean(measures, getter);
+        double stddev = std(measures, getter);
+        double interval95 = 1.96 * stddev / Math.sqrt(measures.size());
+
+        return new MeasuringResult(mean, interval95);
+    }
+
+    public static class MeasuringResult {
+        public final double mean;
+        public final double std;
+
+        public MeasuringResult(double mean, double std) {
+            this.mean = mean;
+            this.std = std;
+        }
     }
 }
