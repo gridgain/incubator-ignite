a52be7f16ad636c0ab3c43b956e2ac45363b72e5
Alexey Kuznetsov <akuznetsov@apache.org>
2019-02-25 09:27:39 +0700

    WC-946 Added redirect from HTTP to HTTPS.

--- modules/web-console/src/main/java/org/apache/ignite/test/WebConsoleLauncher.java
+++ modules/web-console/src/main/java/org/apache/ignite/test/WebConsoleLauncher.java
@@ -65,15 +65,15 @@ public class WebConsoleLauncher extends AbstractVerticle {
 
         // TODO Remove this code after WC-950 will be implemented.
         // Uncomment if you need Vertx to handle static resources.
-        cfg.setWebRoot("modules/web-console/frontend/build");
+        // cfg.setWebRoot("modules/web-console/frontend/build");
 
         // TODO Remove this code after WC-950 will be implemented.
         // Uncomment if you need SSL.
-         cfg
-            .setKeyStore("modules/web-console/web-agent/src/test/resources/server.jks")
-            .setKeyStorePassword("123456")
-            .setTrustStore("modules/web-console/web-agent/src/test/resources/ca.jks")
-            .setTrustStorePassword("123456");
+        // cfg
+        //    .setKeyStore("modules/web-console/web-agent/src/test/resources/server.jks")
+        //    .setKeyStorePassword("123456")
+        //    .setTrustStore("modules/web-console/web-agent/src/test/resources/ca.jks")
+        //    .setTrustStorePassword("123456");
 
         vertx.deployVerticle(new WebConsoleServer(cfg, ignite, auth, cfgsRouter, notebooksRouter, downloadRouter));
 
