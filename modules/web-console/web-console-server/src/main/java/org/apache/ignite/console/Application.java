7fa1ef2f84f6c94ccc588f6c3a84761f96289759
Alexey Kuznetsov <akuznetsov@apache.org>
2019-03-20 18:48:53 +0700

    WC-999 Try spring boot 2

---
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/Application.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
