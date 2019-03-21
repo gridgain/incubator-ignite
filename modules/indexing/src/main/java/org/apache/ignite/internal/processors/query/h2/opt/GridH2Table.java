9a4adf756ba34a4165ac4884b06e7cd00b6f0c6a
EdShangGG <eshangareev@gridgain.com>
2019-03-21 17:30:55 +0300

    GG-14869

--- modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
+++ modules/indexing/src/main/java/org/apache/ignite/internal/processors/query/h2/opt/GridH2Table.java
@@ -565,7 +565,7 @@ public class GridH2Table extends TableBase {
         boolean replaced = idx.putx(row);
 
         // Row was not replaced, need to remove manually.
-        if (!replaced && prevRow != null && !prevRow.key().equals(row.key())) {
+        if (!replaced && prevRow != null) {
             log.error("@@@ REMOVED cacheId=" + row.cacheId() + ", key=" + row.key().hashCode() + ", index="  + idx.getName(), new Throwable(String.valueOf(prevRow)));
 
             idx.removex(prevRow);
