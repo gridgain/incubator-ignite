4ebe5319e5fe9a599a5f126f5afb47aa0f5f02b2
Slava Koptilin <slava.koptilin@gmail.com>
2019-04-01 14:18:57 +0300

    wip

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionsExchangeFuture.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/distributed/dht/preloader/GridDhtPartitionsExchangeFuture.java
@@ -2173,7 +2173,7 @@ public class GridDhtPartitionsExchangeFuture extends GridDhtTopologyFutureAdapte
 
                     }
 
-                    if (!grpToRefresh.isEmpty()) {
+                    if (!grpToRefresh.isEmpty() && !crd.isLocal()) {
                         if (log.isDebugEnabled())
                             log.debug("Refresh partitions due to partitions initialized when affinity ready [" +
                                 grpToRefresh.stream().map(CacheGroupContext::name).collect(Collectors.toList()) + ']');
