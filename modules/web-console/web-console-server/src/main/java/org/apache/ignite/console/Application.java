a37ac45368c7e37d469981b8771c5b23b91aeebf
Andrey Novikov <anovikov@gridgain.com>
2019-04-03 12:54:48 +0700

    WC-1027 Fixed build of docker image for hosted solution.

--- modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/Application.java
+++ modules/web-console/web-console-server/src/main/java/org/apache/ignite/console/Application.java
@@ -1,14 +1,9 @@
 package org.apache.ignite.console;
 
-import org.apache.ignite.Ignition;
-import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.internal.IgniteEx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.ImportResource;
 
 /** */
 @SpringBootApplication
@@ -18,15 +13,5 @@ public class Application {
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    /**
-     *  TODO IGNITE-5617 javadocs.
-     *
-     *  Do we really need this bean here? not in some configuration?
-     */
-    @Bean
-    public IgniteEx authProvider() {
-        return (IgniteEx)Ignition.start(new IgniteConfiguration().setClientMode(true).setPeerClassLoadingEnabled(true));
     }
 }
