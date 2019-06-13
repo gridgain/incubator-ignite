0e0c17a84bf445518953cd58c216f82d1554fd78
ibessonov <bessonov.ip@gmail.com>
2019-06-13 13:17:34 +0300

    GG-19269 Smaller timeout in U.currentTimeMillis calculation.

--- modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/IgniteUtils.java
@@ -3311,7 +3311,7 @@ public abstract class IgniteUtils {
                             curTimeMillis = System.currentTimeMillis();
 
                             try {
-                                Thread.sleep(10);
+                                Thread.sleep(2);
                             }
                             catch (InterruptedException ignored) {
                                 break;
