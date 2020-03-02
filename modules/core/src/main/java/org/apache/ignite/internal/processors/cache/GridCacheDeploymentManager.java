c7aa2d07c4f4462e09988111f062cf071c224809
denis-chudov <moonglloom@gmail.com>
2020-03-02 19:34:45 +0300

    tests

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheDeploymentManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/GridCacheDeploymentManager.java
@@ -684,7 +684,7 @@ public class GridCacheDeploymentManager<K, V> extends GridCacheSharedManagerAdap
             }
 
             if (dep != null) {
-                checkDeployment(dep, deployable);
+                checkDeploymentIsCorrect(dep, deployable);
 
                 deployable.prepare(dep);
             }
@@ -694,13 +694,14 @@ public class GridCacheDeploymentManager<K, V> extends GridCacheSharedManagerAdap
         }
     }
 
-    private void checkDeployment(GridDeploymentInfoBean deployment, GridCacheDeployable deployable) throws IgnitePeerToPeerClassLoadingException {
+    private void checkDeploymentIsCorrect(GridDeploymentInfoBean deployment, GridCacheDeployable deployable)
+        throws IgnitePeerToPeerClassLoadingException {
         if (deployment.participants() == null
             && !cctx.localNode().id().equals(deployment.classLoaderId().globalId())) {
             throw new IgnitePeerToPeerClassLoadingException("Could not use deployment to prepare deployable, " +
                 "because local node id does not correspond with class loader id, and there are no more participants, " +
-                "localNodeId=" + cctx.localNode().id() + ", classLoaderId=" + deployment.classLoaderId() +
-                ", participants=null, deployment=" + deployment + ", deployable=" + deployable);
+                "localNodeId=" + cctx.localNode().id() + ", deployment=" + deployment + ", deployable=" + deployable +
+                ", locDep=" + locDep.get());
         }
     }
 
